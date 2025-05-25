d1dbd3a454f814f442f6feeb1245c9e5b2d264ca
Craig Patik <craig@patik.com>
2025-05-25 22:13:56 +0200

    finish migrating eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
-import { default as pluginJs } from '@eslint/js'
+// @ts-check
+
+import eslint from '@eslint/js'
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import unusedImports from 'eslint-plugin-unused-imports'
 import tseslint from 'typescript-eslint'
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-// const compat = new FlatCompat({
-//     baseDirectory: __dirname,
-//     recommendedConfig: js.configs.recommended,
-//     allConfig: js.configs.all,
-// })
-
-export default [
+export default tseslint.config([
     { files: ['src/**/*.{ts}'] },
     { ignores: ['node_modules/**', 'dist/**'] },
-    pluginJs.configs.recommended,
+    eslint.configs.recommended,
     ...tseslint.configs.recommended,
+    tseslint.configs.eslintRecommended,
     eslintPluginPrettier,
     {
         rules: {
@@ -26,49 +22,23 @@ export default [
             ],
         },
     },
-
-    // { files: ['./src/**/*.{js,mjs,cjs,ts,mts}'], ignores: ['./dist/**/*.{js,mjs,cjs,ts,mts}'] },
-    // {
-    //     extends: fixupConfigRules(
-    //         compat.extends(
-    //             'eslint:recommended',
-    //             'plugin:@typescript-eslint/eslint-recommended',
-    //             'plugin:@typescript-eslint/recommended',
-    //             'plugin:react/recommended',
-    //             'plugin:react-hooks/recommended',
-    //             'plugin:import/warnings',
-    //             'plugin:import/errors',
-    //             'plugin:react/jsx-runtime',
-    //         ),
-    //     ),
-
-    //     plugins: {
-    //         '@typescript-eslint': fixupPluginRules(typescriptEslint),
-    //         'unused-imports': unusedImports,
-    //     },
-
-    //     languageOptions: {
-    //         globals: {
-    //             ...globals.jest,
-    //             ...globals.node,
-    //             ...globals.browser,
-    //         },
-
-    //         parser: tsParser,
-    //     },
-
-    //     settings: {
-    //         react: {
-    //             version: 'detect',
-    //         },
-
-    //         'import/resolver': {
-    //             node: {
-    //                 extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    //             },
-    //         },
-    //     },
-
-    //     rules: {},
-    // },
-]
+    {
+        plugins: {
+            'unused-imports': unusedImports,
+        },
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'off',
+            'unused-imports/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+            'unused-imports/no-unused-imports': 'error',
+        },
+    },
+])
